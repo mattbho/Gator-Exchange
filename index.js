55d9a78129f46a841d47fb20631fe79896fbb2e0
Matt Ho <mtthwbh@gmail.com>
2018-09-20 14:51:10 -0700

    About Me's missing photos

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 5000
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-  res.send("About me!");
+  res.render("about");
 });
 
 
