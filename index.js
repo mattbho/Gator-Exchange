f639b8a25ca8efe804ef8e09f5fc7e118c89cb63
Matt Ho <mtthwbh@gmail.com>
2018-09-20 18:40:02 -0700

    Final About Me Page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 const PORT = process.env.PORT || 5000
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("about");
