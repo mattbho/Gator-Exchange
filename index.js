9d983a119cdaf6179d5795855649be9999897752
Matt Ho <mtthwbh@gmail.com>
2018-09-22 07:16:40 -0700

    Revert "Final About Me Page"
    This reverts commit f639b8a25ca8efe804ef8e09f5fc7e118c89cb63.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 const PORT = process.env.PORT || 5000
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("about");
