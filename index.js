d23dd3f053608028a932ab3cfe08ab69250bc050
Mustafa Khan <mustafa.khan@sfs-wifi-dhcp-10-143-140-13.wifi.sfsu.edu>
2018-09-21 21:31:13 -0700

    modularized functions. added mvc pattern.

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 5000
 
+// set view engine to ej templating engine
+
 app.set("view engine", "ejs");
+
+// use of middleware to server static public directory
+
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("about");
-});
 
+
+// controllers
+
+require('./controllers/about.js')(app)
+
+
+
+// server start at PORT
 
 app.listen(PORT, process.env.IP, function (){
   console.log("Server is listening");
