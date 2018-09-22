346995b55e8e504718b5a97288f087171c81c556
Matt Ho <mtthwbh@gmail.com>
2018-09-22 07:23:55 -0700

    reverted changes back to Matt's commits

--- index.js
+++ index.js
@@ -2,26 +2,15 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 5000
 
-// set view engine to ej templating engine
-
 app.set("view engine", "ejs");
-<<<<<<< HEAD
-=======
-
-// use of middleware to server static public directory
 
 app.use(express.static("public"));
->>>>>>> d23dd3f053608028a932ab3cfe08ab69250bc050
 
 
+app.get("/", function(req, res){
+  res.render("about");
+});
 
-// controllers
-
-require('./controllers/about.js')(app)
-
-
-
-// server start at PORT
 
 app.listen(PORT, process.env.IP, function (){
   console.log("Server is listening");
