ce9ab66e1058d07eb14a18e5c8e0a8663c6009ea
Matt Ho <mtthwbh@gmail.com>
2018-11-12 15:41:17 -0800

    fixed navbar

--- client/src/pages/components/navbar.jsx
+++ client/src/pages/components/navbar.jsx
@@ -4,16 +4,11 @@ class NavBar extends Component {
   state = {};
   render() {
     return (
-      <nav>
-        <ul className="NavBar">
-          <li>
-            <a className="Home" href="/">
-              {" "}
-              Gator Exchange
-            </a>{" "}
-          </li>
+      <ul className = "NavBar">
+          <li><a className = "Home" href = "/"> Gator Exchange</a> </li>
+          <li><a className = "About" href = "About"> About</a></li>
+          <li><a className = "Sell" href = "Sell"> Sell</a></li>           
         </ul>
-      </nav>
     );
   }
 }
