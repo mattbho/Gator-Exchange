70dab45fbc03f719145b361fb36dcbc44ba88ab9
Matt Ho <mtthwbh@gmail.com>
2018-11-11 21:52:48 -0800

    Refactored PostDetails

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -7,12 +7,6 @@ class NavBar extends Component {
       <nav>
         <ul className = "NavBar">
           <li><a className = "Home" href = "/"> Gator Exchange</a> </li>
-          <li className="cats"><a className = "Categories" href = "Categories"> Categories</a></li>
-                  <div className="Dropdown-content">
-                  <a href="/categories/books">Books</a>
-                  <a href="/categories/clothes">Clothes</a>
-                  <a href="/categories/miscellaneous">Miscellaneous</a>
-                  </div>
           <li><a className = "About" href = "About"> About</a></li>
           <li><a className = "Profile" href = "Profile"> Profile</a></li>
           <li><a className = "Sell" href = "Sell"> Sell</a></li>           
--- client/src/pages/PostDetails.js
+++ client/src/pages/PostDetails.js
@@ -1,34 +1,32 @@
 import React from "react";
-import Post from './components/posts';
+import Post from "./components/posts";
 
-export default class PostDetails extends React.Component{
-    state = {
-        query: "Clothes",
-        items: null
-      };
-      search() {
-        
-        fetch(`/api/${this.state.query}`)
-          .then(response => response.json())
-          .then(json => {
-            this.setState({ items: json });
-          });
-      }
+export default class PostDetails extends React.Component {
+  state = {
+    query: "Clothes",
+    items: null
+  };
+  componentDidMount() {
+    fetch(`/api/${this.state.query}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ items: json });
+      });
+  }
 
-    render(){
-        let items = this.state.items;
-        
-        this.search();
-        return(
-            <div>
-            {this.state.items !== null ? (
-                <div>
-                  <Post list = {items}/>
-                </div>
-              ) : (
-                <div/>
-              )}
-            </div>
-        );
-    }
+  render() {
+    let items = this.state.items;
+
+    return (
+      <div>
+        {this.state.items !== null ? (
+          <div>
+            <Post list={items} />
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  }
 }
