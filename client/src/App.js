de36ffb79286fee799a686e4dcc759b481c2733b
sthriza <rshrest1@mail.sfsu.edu>
2018-11-17 17:51:07 -0800

    login and register styling added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import "./styling/navbar.css";
 import "./styling/errorpage.css";
 import "./styling/homepage.css";
 import "./styling/details.css";
+import "./styling/register.css";
+import "./styling/login.css";
+
 
 import NavBar from "./pages/components/navbar";
 import SearchBar from "./pages/components/searchbar";
@@ -17,7 +20,7 @@ import PostDetails from "./pages/PostDetails";
 import SearchResults from "./pages/components/search-results";
 import SellForm from "./pages/components/sell-form";
 import Login from "./pages/components/login";
-import Register from "./pages/components/register"
+import Register from "./pages/components/register";
 
 class App extends Component {
   render() {
--- client/src/pages/components/register.jsx
+++ client/src/pages/components/register.jsx
@@ -4,45 +4,56 @@ class Register extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h3>Register</h3>
-        <form>
-          <div>
-            <label>Username </label>
-            <input type="text" name="Username" />
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-xl-5 mx-auto">
+                <div className="card card-signin flex-row my-5">
+                  <div className="card-body">
+                    <h5 className="card-title text-center">Register</h5>
+                    {/* Form register */}
+                    <form className="form-signin">
+                      <div className="form-label-group">
+                          <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus ></input>
+                          <label for="inputUserame">Username</label>
+                      </div>
+                      <div className="form-label-group">
+                          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required></input>
+                          <label for="inputEmail">Email Address</label>
+                      </div>
+                      <div className="form-label-group">
+                          <input type="number" id="inputID" class="form-control" placeholder="SFSU ID" required></input>
+                          <label for="inputID">SFSU ID</label>
+                      </div>
+              
+                      <hr></hr>
+
+                        <div className="form-label-group">
+                          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
+                          <label for="inputPassword">Password</label>
+                        </div>
+              
+                        <div className="form-label-group">
+                          <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Confirm Password" required></input>
+                          <label for="inputConfirmPassword">Confirm password</label>
+                        </div>
+                      
+                        <button className="btn btn-lg btn-block text-uppercase" type="submit">Register</button>
+                        <Link
+                          to={{
+                           pathname: "/Login"
+                           }}
+                          >
+                           {" "}
+                           <div className = "d-block text-center mt-8 medium">Sign In </div>
+                        </Link>
+                        <hr class="my-4"></hr>     
+                    </form>
+                 </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <label>Email </label>
-            <input type="email" name="Email" />
-          </div>
-          <div>
-            <label>Password </label>
-            <input type="password" name="Password" />
-          </div>
-          <div>
-            <label>Verify Password </label>
-            <input type="password" name="Password" />
-          </div>
-          <div>
-            <label>SFSU ID </label>
-            <input type="number" name="sfsu-id" />
-          </div>
-          <button type="submit">Register</button>
-        </form>
-        <div>
-          <p>
-            Need an account? Log in
-            <Link
-              to={{
-                pathname: "/Login"
-              }}
-            >
-              {" "}
-              here!
-            </Link>
-          </p>
-        </div>
-      </div>
+        </div>  
+  
     );
   }
 }
