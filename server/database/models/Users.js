63a85bcc234fc2635631f6401f365f0c041d5f2b
Matthew Ho <mtthwbh@gmail.com>
2018-11-27 10:27:38 -0800

    Fixed typo on Schema Passowrd

--- server/database/models/Users.js
+++ server/database/models/Users.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   username:{ type: String, unique: true, required: true},
-  password: { type: String, unique: false, require: true}
+  password: { type: String, unique: false, required: true}
 });
 
 UserSchema.methods = {
