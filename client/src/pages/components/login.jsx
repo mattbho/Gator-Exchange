e20b7a295faded2c12cb9e57de3a0553a9e3d000
Matthew Ho <mtthwbh@gmail.com>
2018-12-03 15:11:30 -0800

    Revert "Front end styling"

--- client/src/pages/components/login.jsx
+++ client/src/pages/components/login.jsx
@@ -4,63 +4,32 @@ class Login extends Component {
   state = {};
   render() {
     return (
-      <body className="my-login-page">
-	    <section className="h-100">
-		  <div className="container h-100">
-			  <div className="row justify-content-md-center h-100">
-				  <div className="card-wrapper">
-					  <div className="card fat">
-						  <div className="card-body">
-							<h4 className="card-title">Login</h4>
-							{/* Requires validation still needs to be done */}
-							<form method="POST" className="my-login-validation" novalidate="">
-								  <div className="form-group">
-									  <label for="email">E-Mail Address</label>
-									  <input id="email" type="email" className="form-control" name="email" required autofocus></input>
-									      <div className="invalid-feedback">
-									      	Email is invalid
-									      </div>
-								  </div>
-									{/* Requires forget password still needs to be  */}
-								  <div className="form-group">
-									    <label for="password">Password
-										  <a href="forgot.html" className="float-right">
-											  Forgot Password?
-									    	</a>
-									    </label>
-									    <input id="password" type="password" className="form-control" name="password" required data-eye></input>
-								          <div className="invalid-feedback">
-								    	        Password is required
-							    	      </div>
-								  </div>
-
-								<div className="form-group m-0">
-									<button type="submit" className="btn btn-block">
-										Login
-									</button>
-								</div>
-                <div className = "d-block text-center mt-8 medium"> Need an Account? 
-								    <Link
-                        to={{
-                        pathname: "/Register"
-                        }}
-                        >
-                        {" "}
-                        Register
-                    </Link>
-                </div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<script src="js/jquery.min.js"></script>
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<script src="js/my-login.js"></script>
-</body>
+      <div>
+        <h3>Login</h3>
+        <form>
+          <div>
+            <label>Username/Email</label>
+            <input type="text" name="Username" required />
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="text" name="Password" required />
+          </div>
+        </form>
+        <div>
+          <p>
+            Need an account? Register
+            <Link
+              to={{
+                pathname: "/Register"
+              }}
+            >
+              {" "}
+              here!
+            </Link>
+          </p>
+        </div>
+      </div>
     );
   }
 }
--- client/src/pages/components/sell-form.jsx
+++ client/src/pages/components/sell-form.jsx
@@ -1,62 +1,42 @@
 import React, { Component } from "react";
-import { } from 'bootstrap';
 
 export default class SellForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-/*
-Styling By: Juan Valdez
-The whole point of the sell-form styling is to create an appealing sell form 
-where it is presentable and easy to use for a user. A card styl fill out form
-without the upload image function and backend connection.
-*/
-
   render() {
     return (
-      //NEEDS UPLOAD IMAGE AND CONNECTION TO BACK END
-      <div className="row justify-content-md-center h-100">
-        <div className="card-wrapper w-50">
-          <div className="card fat">
-            <div className="card-body">
-              <form>
-                <div class=" form-group container-fluid">
-                  <title>Create Listing/Sell</title>
-                  <div class="row justify-content-md-center">
-                    <h4 className="card-title">Create Listing/Sell</h4>
-                  </div>
-                  <div class="form-group row justify-content-md-center">
-                    <div class="col-md-1.5">
-                      <select class="custom-select">
-                        <option selected>Select Category</option>
-                        <option value="Books">Books</option>
-                        <option value="Car">Car</option>
-                        <option value="Clothing">Clothing</option>
-                        <option value="Misc">Miscellaneous</option>
-                        <option value="Supplies">Supplies</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="form-row justify-content-md-center">
-                    <label for="Item-Description">Item Description: </label>
-                  </div>
-                  <div class="form-group row justify-content-md-center">
+      <div>
+        <title>Create Listing/Sell</title>
+        <form>
+          <div>
+            <label for="Category">Item Category:</label>
 
-                    <textarea class="form-control w-50" id="description" placeholder="Item Description"></textarea>
-                  </div>
-                  <div class="form-row justify-content-md-center">
-                    <label for="Contact">Contact Information: </label>
-                  </div>
-
-                  <div class="form-group row justify-content-md-center">
-                    <textarea class="form-control w-50" id="contact" placeholder="Contact Information"></textarea>
-                  </div>
-                </div>
-              </form>
-            </div>
+            <select name="Categories">
+              <option value="Books">Books</option>
+              <option value="Car">Car</option>
+              <option value="Clothing">Clothing</option>
+              <option value="Misc">Miscellaneous</option>
+              <option value="Supplies">School Supplies</option>
+            </select>
           </div>
-        </div>
+          <div>
+            <label for="Description">Item Description:</label>
+
+            <textarea id="Description" name="Description" />
+          </div>
+
+          <div>
+            <label for="Contact">Contact Information:</label>
+            <textarea id="Contact" name="Contact" />
+          </div>
+
+          <div>
+            <label for="Price">Price:</label>
+            <textarea id="Price" name="Price" />
+          </div>
+        </form>
       </div>
     );
   }
